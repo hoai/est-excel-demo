440f3a1fd1f02adbb7989867223cd90b40413c38
Monkey Jin <monkey@estgroupe.com>
2016-03-25 22:33:12 +0800

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,23 +12,24 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    /*
-     *  解析 Excel
-     */
+
     public function import() {
         $file_name = public_path().'/excel/job.xlsx';
-        $excel_data = Excel::load($file_name)->get()->toArray();
+        $excel_data = Excel::load($file_name, function($reader) {
+            $reader = $reader->getSheet(0);
+
+            echo '"job.xlsx" Content is:';
+            dd($reader->toArray());
+        });
     }
 
-    /*
-     *  将数据导出成 Excel
-     */
+
     public function export() {
         $file_name = time();
         Excel::create($file_name, function ($excel) {
             $excel->sheet('Sheetname', function ($sheet) {
                 // 设置 excel 的 header
-                $header_row = ['姓名', '年龄'];
+                $header_row = ['Name', 'age'];
                 $sheet->appendRow($header_row);
 
                 foreach ($this->create_temp_data() as $user) {
@@ -43,12 +44,12 @@ class Controller extends BaseController
 
     public function create_temp_data() {
         $user_data = [];
-        $user_data[] = ['name' => '张三', 'age' => '15'];
-        $user_data[] = ['name' => '李四', 'age' => '16'];
-        $user_data[] = ['name' => '王五', 'age' => '17'];
-        $user_data[] = ['name' => '小明', 'age' => '18'];
-        $user_data[] = ['name' => '小红', 'age' => '19'];
-        $user_data[] = ['name' => '小军', 'age' => '20'];
+        $user_data[] = ['name' => 'Li', 'age' => '15'];
+        $user_data[] = ['name' => 'Wang', 'age' => '16'];
+        $user_data[] = ['name' => 'Min', 'age' => '17'];
+        $user_data[] = ['name' => 'Monkey', 'age' => '18'];
+        $user_data[] = ['name' => 'Connie', 'age' => '19'];
+        $user_data[] = ['name' => 'Apple', 'age' => '20'];
 
         return $user_data;
     }
